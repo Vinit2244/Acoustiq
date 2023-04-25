ef525cf8a59640d3625d1742a11b365222e589e4
Vinit Mehta <vinit2004mehtaa@gmail.com>
2023-04-25 19:12:12 +0530

    Added the typing effect on about page and rectified the nav-bar font issue and made a few minute changes to spotlight-page

--- src/scripts/spotlight.js
+++ src/scripts/spotlight.js
@@ -59,7 +59,7 @@ function addReview() {
       userCell.style.backgroundColor = "rgba(0, 109, 217, 0.2)";
       ratingCell.innerHTML = `<svg viewBox="0 0 24 24"><path d="M12,17.5C14.33,17.5 16.3,16.04 17.11,14H6.89C7.69,16.04 9.67,17.5 12,17.5M8.5,11A1.5,1.5 0 0,0 10,9.5A1.5,1.5 0 0,0 8.5,8A1.5,1.5 0 0,0 7,9.5A1.5,1.5 0 0,0 8.5,11M15.5,11A1.5,1.5 0 0,0 17,9.5A1.5,1.5 0 0,0 15.5,8A1.5,1.5 0 0,0 14,9.5A1.5,1.5 0 0,0 15.5,11M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2C6.47,2 2,6.5 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" style="fill: rgb(0, 109, 217);"/></svg>`
     }
-    reviewCell.innerHTML = `&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;${'- ' + review}`;
+    reviewCell.innerHTML = `&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;&NonBreakingSpace;${'~ ' + review}`;
     userCell.innerHTML = `&NonBreakingSpace;&NonBreakingSpace;${userName}`;
 
     var temprow2 = reviewsTableBody.insertRow(-1);
@@ -188,22 +188,22 @@ window.addEventListener('load', function() {
   var image = document.getElementById('artist-image');
   var currentWidth = 900;
   var targetWidth = 500;
-  var transitionTime = 3000; // 5 seconds
+  var transitionTime = 3000;
 
-  image.style.width = currentWidth + 'px';  // set initial size to 600x600
+  image.style.width = currentWidth + 'px';
 
   var startTime = performance.now();
 
   function animate() {
     var elapsed = performance.now() - startTime;
-    var progress = Math.min(elapsed / transitionTime, 1); // ensure progress is between 0 and 1
-    var newWidth = currentWidth + (targetWidth - currentWidth) * progress; // interpolate between current and target widths
+    var progress = Math.min(elapsed / transitionTime, 1);
+    var newWidth = currentWidth + (targetWidth - currentWidth) * progress;
     image.style.width = newWidth + 'px';
 
     if (progress < 1) {
-      requestAnimationFrame(animate); // continue animating
+      requestAnimationFrame(animate);
     }
   }
 
-  requestAnimationFrame(animate); // start the animation
+  requestAnimationFrame(animate);
 });
