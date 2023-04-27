03d27276dee3f2531006885c3b1b87dcbf1647a8
cookiepresence <127502417+cookiepresence@users.noreply.github.com>
2023-04-27 20:06:08 +0530

    modify to retain color after mouse left

--- src/scripts/hover.js
+++ src/scripts/hover.js
@@ -1,3 +1,8 @@
-document.querySelectorAll("ul.navigation-options > li > a").forEach((el)=> el.onmouseover = function (event) {
+document.querySelectorAll("ul.navigation-options > li > a").forEach((el)=> el.onmouseenter = function (event) {
+    event.target.prev_color = event.target.style.color;
     event.target.style.color = "blanchedalmond";
+})
+
+document.querySelectorAll("ul.navigation-options > li > a").forEach((el)=> el.onmouseleave = function (event) {
+    event.target.style.color = event.target.prev_color;
 })
